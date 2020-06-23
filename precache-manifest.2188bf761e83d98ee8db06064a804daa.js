2783a6b893daf8a47aff552296bb85a55b703abd
Jose Naime <34799483+JoseNaime@users.noreply.github.com>
2020-06-23 14:44:07 -0500

    Updates

---
+++ precache-manifest.2188bf761e83d98ee8db06064a804daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8df9465753b98bc82e3e827625084c0",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7976687e02c4e055bba3",
+    "url": "/RoboFriends/static/css/main.14e34fc4.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7976687e02c4e055bba3",
+    "url": "/RoboFriends/static/js/main.172f9838.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
